7d2d8da23e3106d95ac06c24faa37f41b1aab862
iloktionov <iloktionov@skbkontur.ru>
2020-02-04 19:07:07 +0500

    Added a stub of VostokScheduledApplication.

---
+++ Vostok.Applications.Scheduled/VostokScheduledApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Vostok.Hosting.Abstractions;
+
+namespace Vostok.Applications.Scheduled
+{
+    [PublicAPI]
+    public class VostokScheduledApplication : IVostokApplication
+    {
+        public Task InitializeAsync(IVostokHostingEnvironment environment) =>
+            throw new NotImplementedException();
+
+        public Task RunAsync(IVostokHostingEnvironment environment) =>
+            throw new NotImplementedException();
+    }
+}
