022c4e47058cbc4405049603113ac0d9101dcf10
iloktionov <iloktionov@skbkontur.ru>
2020-05-29 01:39:36 +0500

    Implemented AddScheduled extension for ICompositeApplicationBuilder.

---
+++ Vostok.Applications.Scheduled/ICompositeApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Hosting.Abstractions;
+using Vostok.Hosting.Abstractions.Composite;
+
+namespace Vostok.Applications.Scheduled
+{
+    [PublicAPI]
+    public static class ICompositeApplicationBuilderExtensions
+    {
+        [NotNull]
+        public static ICompositeApplicationBuilder AddScheduled(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IScheduledActionsBuilder, IVostokHostingEnvironment> setup)
+            => builder.AddApplication(new AdHocScheduledApplication(setup));
+
+        [NotNull]
+        public static ICompositeApplicationBuilder AddScheduled(
+            [NotNull] this ICompositeApplicationBuilder builder,
+            [NotNull] Action<IScheduledActionsBuilder> setup)
+            => builder.AddApplication(new AdHocScheduledApplication((b, _) => setup(b)));
+
+        private class AdHocScheduledApplication : VostokScheduledApplication
+        {
+            private readonly Action<IScheduledActionsBuilder, IVostokHostingEnvironment> setup;
+
+            public AdHocScheduledApplication(Action<IScheduledActionsBuilder, IVostokHostingEnvironment> setup)
+                => this.setup = setup;
+
+            public override void Setup(IScheduledActionsBuilder builder, IVostokHostingEnvironment environment)
+                => setup(builder, environment);
+        }
+    }
+}
