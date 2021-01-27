4634c5dd565721cbe7bc99abe0fdb361fa5d2425
unknown <tolya12345w@gmail.com>
2021-01-27 10:13:33 +0500

    Removed DoDispose and added access modifiers

--- Vostok.Applications.Scheduled/VostokScheduledApplication.cs
+++ Vostok.Applications.Scheduled/VostokScheduledApplication.cs
@@ -9,11 +9,28 @@ namespace Vostok.Applications.Scheduled
     {
         public abstract void Setup([NotNull] IScheduledActionsBuilder builder, [NotNull] IVostokHostingEnvironment environment);
 
-        public override Task SetupAsync(IScheduledActionsBuilder builder, IVostokHostingEnvironment environment)
+        public sealed override Task SetupAsync(IScheduledActionsBuilder builder, IVostokHostingEnvironment environment)
         {
             Setup(builder, environment);
-            
+
             return Task.CompletedTask;
+        }
+
+        public virtual void DoDispose()
+        {
+        }
+
+        public virtual Task DoDisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected sealed override async Task DisposeAsync()
+        {
+            await DoDisposeAsync().ConfigureAwait(false);
+
+            // ReSharper disable once MethodHasAsyncOverload
+            DoDispose();
         }
     }
 }
