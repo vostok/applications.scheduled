011207a8785e100547479f5b6e44a175276aada1
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-02-18 12:57:01 +0500

    Style changes

--- Vostok.Applications.Scheduled/Helpers/ScheduledApplicationHelper.cs
+++ Vostok.Applications.Scheduled/Helpers/ScheduledApplicationHelper.cs
@@ -11,6 +11,20 @@ namespace Vostok.Applications.Scheduled.Helpers
     {
         public static async Task<(ScheduledActionsRunner, List<IDisposable>)> InitializeAsync(
             IVostokHostingEnvironment environment,
+            Action<IScheduledActionsBuilder, IVostokHostingEnvironment> setupRunner)
+        {
+            return await InitializeAsync(
+                    environment,
+                    (builder, env) =>
+                    {
+                        setupRunner(builder, env);
+                        return Task.CompletedTask;
+                    })
+               .ConfigureAwait(false);
+        }
+        
+        public static async Task<(ScheduledActionsRunner, List<IDisposable>)> InitializeAsync(
+            IVostokHostingEnvironment environment,
             Func<IScheduledActionsBuilder, IVostokHostingEnvironment, Task> setupRunner)
         {
             var builder = new ScheduledActionsBuilder(environment.Log, environment.Tracer);
